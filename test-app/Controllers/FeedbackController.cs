f4b1577cd2d6702e8448e7d4770d89ef3cd4a03d
elizaveta.raspopa <elizaveta.raspopa@RASPOPA10P02>
2018-02-05 21:04:41 +0300

    Add project files.

---
+++ test-app/Controllers/FeedbackController.cs
@@ -0,0 +1,106 @@
+﻿using MvcDataViews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDataViews.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        static List<Feedback> feedback = new List<Feedback>()
+        { new Feedback() {Name="Stranger", Email="someone@example.com", Comment="Awesome!"},
+            new Feedback() {Name="Stranger2", Email="example@example.com", Comment="Cute!"}
+        };
+        // GET: Person
+        public ActionResult Index() { return View(feedback); }
+
+
+        // GET: Person/Details/5
+        public ActionResult Details(Feedback p) { return View(p); }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        public ActionResult Create(Feedback p)
+        {
+            if (!ModelState.IsValid) { return View("Create", p); }
+
+            feedback.Add(p);
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Feedback p = new Feedback(); foreach (Feedback fb in feedback)
+            {
+                if (fb.Id == id)
+                {
+                    p.Name = fb.Name;  p.Id = fb.Id;
+                    p.Email = fb.Email; p.Comment = fb.Comment;
+                }
+            }
+
+            return View(p);
+        }
+
+        // POST: Person/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Feedback p)
+        {
+            if (!ModelState.IsValid) { return View("Edit", p); }
+
+            foreach (Feedback pn in feedback) { if (pn.Id == p.Id) { pn.Name = p.Name; pn.Id = p.Id; pn.Comment = p.Comment; pn.Email = p.Email; } }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Person/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Feedback p = new Feedback();
+            foreach (Feedback fb in feedback) {
+                if (fb.Id == id)
+                {
+                    p.Name = fb.Name;
+                    p.Id = fb.Id;
+                    p.Email = fb.Email;
+                    p.Comment = fb.Comment;
+                }
+            }
+
+            return View(p);
+        }
+
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Feedback p)
+        {
+            try
+            {                 // TODO: Add delete logic here            
+                foreach (Feedback fb in feedback)
+                {
+                    if (fb.Id == p.Id)
+                    {
+                        feedback.Remove(fb);
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(p);
+            }
+        } 
+
+            }
+}
---
+++ test-app/Models/Feedback.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcDataViews.Models
+{
+    public class Feedback
+    {
+        [Required(ErrorMessage = "The ID is required.")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The name is required.")]
+        public string Name { get; set; }
+
+        
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The comment is required.")]
+        public string Comment { get; set; }
+
+    }
+}
